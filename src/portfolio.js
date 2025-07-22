3da93a9d2ee2516208ac51959bc6ca2189d7ba7a
Mohanad Mazin <mmazin@skatescribe.com>
2025-07-22 12:53:17 -0400

    homepage change

--- src/portfolio.js
+++ src/portfolio.js
@@ -308,7 +308,6 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+1-437-996-2955",
   email_address: "mohanad.mazin188@hotmail.com"
 };
 
